69ce45375c35c8a898d056ab7e73ab79eb4087e0
GameDevVaibhav <147718788+GameDevVaibhav@users.noreply.github.com>
2023-10-31 18:05:37 +0530

    Bike and Car collision added

--- The Chase/Assets/Script/PoliceCarCollision.cs
+++ The Chase/Assets/Script/PoliceCarCollision.cs
@@ -4,12 +4,27 @@ using UnityEngine;
 
 public class PoliceCarCollision : MonoBehaviour
 {
+    private int collisionCount = 0;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("PoliceCar"))
         {
             Destroy(gameObject);
         }
-        
+
+        if (collision.gameObject.CompareTag("PoliceBike"))
+        {
+            collisionCount++; // Increment the collision count when colliding with a police bike.
+
+            // Check if the collision count reaches 3, and destroy the police car if it does.
+            if (collisionCount >= 3)
+            {
+                Destroy(gameObject);
+                
+                collisionCount = 0;
+            }
+        }
     }
+
+
 }
