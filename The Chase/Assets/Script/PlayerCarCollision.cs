c608c90f9d618a0d506bfa2e79b4fc363f48cdb3
GameDevVaibhav <147718788+GameDevVaibhav@users.noreply.github.com>
2023-11-04 23:24:02 +0530

    Swat car damage improved

--- The Chase/Assets/Script/PlayerCarCollision.cs
+++ The Chase/Assets/Script/PlayerCarCollision.cs
@@ -9,6 +9,7 @@ public class PlayerCarCollision : MonoBehaviour
     private float timeBetweenCollisions = 4f; // Set the time limit for resetting health here.
     private float swatCarDamageInterval = 1.0f; // Time interval to reduce health if a SwatCar is in the scene.
     private float lastSwatCarDamageTime = 0f;
+    private float swatCarDamage;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -52,14 +53,23 @@ public class PlayerCarCollision : MonoBehaviour
     private bool SwatCarIsInScene()
     {
         GameObject[] swatCars = GameObject.FindGameObjectsWithTag("SwatCar");
-        return swatCars.Length > 0;
+        if (swatCars.Length == 2)
+        {
+            swatCarDamage = 0.15f * 2;
+        }
+        if (swatCars.Length == 1)
+        {
+            swatCarDamage = 0.15f;
+        }
+
+        return swatCarDamage> 0;
     }
 
     private void ReduceHealthForSwatCar()
     {
         if (Time.time - lastSwatCarDamageTime >= swatCarDamageInterval)
         {
-            playerHealth -= 0.3f; // Reduce health by 0.5 every second when a SwatCar is in the scene.
+            playerHealth -= swatCarDamage; // Reduce health by 0.5 every second when a SwatCar is in the scene.
             lastSwatCarDamageTime = Time.time; // Update the last time health was reduced.
             Debug.Log("Player Health Reduced by SwatCar: " + playerHealth);
         }
