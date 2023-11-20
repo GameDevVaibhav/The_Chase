a5b005ff43cd4b0f7077d3b56e8eb92d8e6c486d
GameDevVaibhav <147718788+GameDevVaibhav@users.noreply.github.com>
2023-11-20 23:54:46 +0530

    Color Chnage Buttons added in menu

--- The Chase/Assets/Script/MainMenu.cs
+++ The Chase/Assets/Script/MainMenu.cs
@@ -7,16 +7,57 @@ using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
     public Button playButton;
+    public Button colorButton;
+    public GameObject colorSelectPanel;
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+
+    private bool colorPanelActive = false;
+
+    private Camera mainCamera; // Reference to the main camera.
 
     private void Start()
     {
-        // Hook up the button click event to the PlayGame method.
+        // Find the main camera during runtime.
+        mainCamera = Camera.main;
+
+        // Hook up the button click events to the corresponding methods.
         playButton.onClick.AddListener(PlayGame);
+
+        colorButton.onClick.AddListener(ToggleColorSelectPanel);
+
+        button1.onClick.AddListener(() => ChangeCameraColor("#FFD966"));
+        button2.onClick.AddListener(() => ChangeCameraColor("#FF8F8F"));
+        button3.onClick.AddListener(() => ChangeCameraColor("#A8DF8E"));
+        button4.onClick.AddListener(() => ChangeCameraColor("#00FFF0"));
     }
 
     private void PlayGame()
     {
         // Load the gameplay scene when the Play button is clicked.
         SceneManager.LoadScene("Chase"); // Replace "GameplayScene" with the actual name of your gameplay scene.
+    }
+
+    private void ToggleColorSelectPanel()
+    {
+        colorPanelActive = !colorPanelActive;
+        colorSelectPanel.SetActive(colorPanelActive);
+    }
+
+    private void ChangeCameraColor(string hexColor)
+    {
+        Color newColor;
+        ColorUtility.TryParseHtmlString(hexColor, out newColor);
+        // Change the background color of the main camera.
+        if (mainCamera != null)
+        {
+            mainCamera.backgroundColor = newColor;
+        }
+        else
+        {
+            Debug.LogWarning("Main camera not found.");
+        }
     }
 }
